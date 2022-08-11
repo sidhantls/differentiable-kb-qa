18f15bc773674c4cf61fe062e8dd1d241a95f87a
sid-sundrani <sidhant96@outlook.com>
2022-08-11 13:45:59 +0530

    add inverted edge

--- kgqa/utils/data_utils.py
+++ kgqa/utils/data_utils.py
@@ -2,11 +2,16 @@ import os
 import re
 import torch
 
-def load_triplets(path: str):   
+def load_triplets(path: str, keep_only_entities: dict = {}):   
     """
     Given path of a text file of knowledge graph, load head, rel, tail relations
 
     Follows the format of MetaQA, kg.txt
+    
+    Inputs:
+        - path to file 
+        - keep_only_entities - contains dict of those entities that we want to keep in KB. This is only useful 
+            for aligning the large metaqa with the new updated metaqa dataset. Keep this empty for all other usecases
 
     Returns:
         - triplets: list of tuples (headid, relid, tailid)
@@ -28,6 +33,10 @@ def load_triplets(path: str):
             line = line.strip()
             head, rel, tail = line.split('|')
             
+            # only for special case
+            if keep_only_entities and (head not in keep_only_entities or tail not in keep_only_entities):
+                continue
+                
             # add entity idx to mapping
             for ent in [head, tail]: 
                 if ent not in entity_to_idx:
@@ -43,6 +52,7 @@ def load_triplets(path: str):
             # create triplet (head_idx, relation_idx, tail_idx)
             curr_head_idx, curr_rel_idx, curr_tail_idx = entity_to_idx[head], rel_to_idx[rel], entity_to_idx[tail]
             
+            
             triplets.append((curr_head_idx, curr_rel_idx, curr_tail_idx))
             
     print('num entities:', len(entity_to_idx))
@@ -53,6 +63,79 @@ def load_triplets(path: str):
     idx_to_rel = {idx: rel for rel, idx in rel_to_idx.items()}
 
     return triplets, entity_to_idx, rel_to_idx, idx_to_entity, idx_to_rel
+
+
+
+def load_triplets_metaqa(path: str, not_inverse_fields = ['release_year', 'in_language', 'has_genre', 'has_tags']):   
+    """
+    Given path of a text file of knowledge graph, load head, rel, tail relations
+
+    Follows the format of MetaQA, kg.txt
+    
+    Inputs:
+        - path to file 
+        - keep_only_entities - contains dict of those entities that we want to keep in KB. This is only useful 
+            for aligning the large metaqa with the new updated metaqa dataset. Keep this empty for all other usecases
+
+    Returns:
+        - triplets: list of tuples (headid, relid, tailid)
+        - entity_to_idx- entity name to idx dict
+        - rel_to_idx - relation name to idx dict
+        - idx_to_entity
+        - idx_to_rel
+    """
+    entity_to_idx, rel_to_idx = {}, {}
+    ent_idx, rel_idx = -1, -1
+
+    triplets = []
+
+    if not os.path.exists(path):
+        print(f'Path {path} does not exist')
+
+    with open(path, 'r', encoding="utf8") as f: 
+        for line in f:
+            line = line.strip()
+            head, rel, tail = line.split('|')
+
+            # add entity idx to mapping
+            for ent in [head, tail]: 
+                if ent not in entity_to_idx:
+                    ent_idx += 1
+                    entity_to_idx[ent] = ent_idx
+                
+            
+            # add relation idx to mapping
+            if rel not in rel_to_idx: 
+                rel_idx += 1
+                rel_to_idx[rel] = rel_idx            
+            
+            # create triplet (head_idx, relation_idx, tail_idx)
+            curr_head_idx, curr_rel_idx, curr_tail_idx = entity_to_idx[head], rel_to_idx[rel], entity_to_idx[tail]
+            
+            triplets.append((head, rel, tail))
+            
+            # add inverse relationship
+            # according to benchmark - https://github.com/google-research/language/blob/master/language/emql/preprocess/metaqa_preprocess.py#L63
+             
+            if rel not in not_inverse_fields:
+                rel = rel + '-inv'
+                triplets.append((tail, rel, head))
+                
+                if rel not in rel_to_idx: 
+                    rel_idx += 1
+                    rel_to_idx[rel] = rel_idx
+                
+            
+            
+    print('num entities:', len(entity_to_idx))
+    print('num relations:', len(rel_to_idx))
+    print('num triplets ', len(triplets))
+
+    idx_to_entity = {idx: ent for ent, idx in entity_to_idx.items()}
+    idx_to_rel = {idx: rel for rel, idx in rel_to_idx.items()}
+
+    return triplets, entity_to_idx, rel_to_idx, idx_to_entity, idx_to_rel
+
 
 
 def load_qa_pairs(path):
@@ -145,10 +228,11 @@ def create_differentiable_kg(triplets, entity_to_idx, rel_to_idx):
     """
     Create the 3 matrices for differentiable kg - Ms, Mo, Mr
     """
+    triplet_ids = [(entity_to_idx[row[0]], rel_to_idx[row[1]], entity_to_idx[row[2]]) for row in triplets]
     
-    Ms = create_sparse_tensor(entity_to_idx, triplets, name='head')
-    Mr = create_sparse_tensor(rel_to_idx, triplets, name='relation')
-    Mo = create_sparse_tensor(entity_to_idx, triplets, name='tail')
+    Ms = create_sparse_tensor(entity_to_idx, triplet_ids, name='head')
+    Mr = create_sparse_tensor(rel_to_idx, triplet_ids, name='relation')
+    Mo = create_sparse_tensor(entity_to_idx, triplet_ids, name='tail')
     
     return Ms, Mr, Mo
 
